734c0c22ab8d7264fbb397fbb41dc920bed38f55
rozycos <rozycos@gmail.com>
2024-02-07 22:08:44 +0100

    progress in navigation

--- components/shared/Header/Navigation/Navigation.js
+++ components/shared/Header/Navigation/Navigation.js
@@ -1,11 +1,35 @@
-import { HamburgerNav } from "./NavElements/HamburgerNav"
-import { NavBar } from "./NavElements/NavBar"
+"use client"
+import { HamburgerNav } from "./NavElements/HamburgerNav";
+import { NavBar } from "./NavElements/NavBar";
+import { useState, useEffect } from 'react';
 
 export const Navigation =()=>{
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+    useEffect(() => {
+        const handleResize = () => {
+          setIsSmallScreen(window.innerWidth <= 768);
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        handleResize(); 
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+    
     return(
         <>
-            <NavBar/>
-            <HamburgerNav/>
+                  {isSmallScreen ? (
+        /* Renderuj hamburger menu */
+        <HamburgerNav />
+      ) : (
+        /* Renderuj zwykłą nawigację */
+        <NavBar />
+      )}
+            {/* <NavBar/>
+            <HamburgerNav/> */}
         </>
     )
 }
